1d2665f04be4e0c58520b798e4f60b1410df2dac
Muhammad Nabeel <nabeelshakeel966@gmail.com>
2024-08-07 16:42:47 +0500

    case bora navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
+import Phone from "@/components/Phone";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
 
@@ -13,7 +14,7 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/snake-1.png" className="w-full" />
+                <img src="snake-1.png" className="w-full" />
 
 
               </div>
@@ -55,9 +56,29 @@ export default function Home() {
                   <img src="users/user-4.jpg" alt="" className="inline-block w-10 h-10 rounded-full ring-2 rign-slate-100" />
                   <img src="users/user-5.jpg" alt="" className="inline-block object-cover w-10 h-10 rounded-full ring-2 rign-slate-100" />
                 </div>
-                
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0 5">
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"></Star>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"></Star>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"></Star>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"></Star>
+                    <Star className="h-4 w-4 text-green-600 fill-green-600"></Star>
+
+                  </div>
+                  <p><span className="font-semibold">
+                    1,250
+                  </span> happy customers</p>
+                </div>
+
               </div>
 
+            </div>
+          </div>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img src="your-image.png" alt="" className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block" />
+              <img src="line.png" alt="" className="absolute w-20 left-6 bottom-6 select-none" />
+              <Phone className="w-64" imgSrc="testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
