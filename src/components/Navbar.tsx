53d4483c31067285e0905f06cd6aa4daa52e7f38
Muhammad Nabeel <nabeelshakeel966@gmail.com>
2024-08-08 14:37:15 +0500

    sign in using kinde

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,12 @@ import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "./ui/button"
 import { ArrowRight } from "lucide-react"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 
-const Navbar = () => {
-    const user = undefined
-    const isAdmin = false
+const Navbar = async () => {
+    const {getUser } = getKindeServerSession()
+    const user = await getUser()
+    const isAdmin = user?.email=== process.env.ADMIN_EMAIL
 
     return (
         <nav className="sticky z-[100]  inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -34,7 +36,7 @@ const Navbar = () => {
                                     }
                                     <Link href='/configure/upload' className={buttonVariants({
                                         size: 'sm',
-                                        variant: 'ghost',
+                                        // variant: 'ghost',
                                         className: ' hidden sm:flex items-center gap-1'
                                     })}>
                                         Create case
